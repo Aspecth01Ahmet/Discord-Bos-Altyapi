0dd141a19afd6f80524215d8266ab53f8419716f
Aspecth01Ahmet <76404183+Aspecth01Ahmet@users.noreply.github.com>
2020-12-22 23:15:43 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+{
+  "name": "hello-express",
+  "version": "0.0.1",
+  "description": "",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "discord.js": "^12.5.0",
+    "ms": "^2.1.2",
+    "fs": "^0.0.2",
+    "node-superfetch": "^0.1.10",
+    "parse-ms": "^2.1.0",
+    "quick.db": "^7.1.2",
+    "chalk": "^4.1.0",
+    "moment": "^2.29.1",
+    "jimp": "^0.16.1",
+    "request": "^2.88.2",
+    "moment-duration-format": "^2.3.2"
+  },
+  "engines": {
+    "node": "12.x"
+  },
+  "repository": {
+    "url": "https://glitch.com/edit/#!/hello-express"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "express"
+  ]
+}
